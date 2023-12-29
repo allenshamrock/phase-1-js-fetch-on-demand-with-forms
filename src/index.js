6b5533b55d45661e014daa62208781f87160aad3
Allen <allenshamrock37@gmail.com>
2023-12-29 15:04:46 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+  let form = document.querySelector("form");
+  let userInput = document.querySelector("searchByID");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let userInput = event.target.searchByID.value;
+    fetch(`http://localhost:3000/movies/${userInput}`)
+      .then((response) => response.json())
+      .then((data) => {
+        let h4 = document.querySelector("h4");
+        let p = document.querySelector("p");
+        p.innerHTML = `Summary:${data.summary}`;
+        h4.innerHTML = `Title:${data.title}`;
+      });
 
-document.addEventListener('DOMContentLoaded', init);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
+
